8b652c0f8be7c3d87278f5d7231324388666bb3b
SrzzuareS <srzzuares@gmail.com>
2023-01-27 01:17:03 -0600

    finish

--- 1.-PrismaBasico/Serve.js
+++ 1.-PrismaBasico/Serve.js
@@ -1,7 +1,7 @@
 const { PrismaClient } = require('@prisma/client')  //Importación
 const prisma = new PrismaClient()   //Inicializamos 
 
-//Funcion de CRUD para Prisma
+//Funcion de CRUD para Prisma https://www.prisma.io/client
 async function main() {
     //Crear Un User
     /* const post = await prisma.users.create({
@@ -18,8 +18,8 @@ async function main() {
             { nameUser: 'Pacheco', emailUser: 'qwerty@example.com' }
         ]
     })
-    console.log(post)
-    */
+    console.log(post) */
+
 
     //Obtener todos los users
     const allUsers = await prisma.users.findMany()
@@ -37,13 +37,47 @@ async function main() {
 
 
 
-    //
+    //Actualizar un dato de la tabla
+    /* const updateUser = await prisma.users.update({
+        where: {
+            id: 2
+        },
+        data: {
+            nameUser: 'Panchito',
+            emailUser: 'pancho@changed.com'
+        }
+    })
+    console.log(updateUser) */
+
+    //Actualiza todos los usuarios que tenga el mismo correo
+    /* const updateUserMany = await prisma.users.updateMany({
+        where: {
+            emailUser: 'qwerty@example.com'
+        },
+        data: {
+            emailUser: 'actualizado@gmail.com'
+        }
+    })
+    console.log(updateUserMany) */
+
+
+    //Eliminar un usuario
+    /* const deleteUSer = await prisma.users.delete({
+        where: {
+            id: 1
+        }
+    })
+    console.log(deleteUSer) */
 
 
 
-
-
-
+    //Eliminar demaciados usuario repetidos
+    /* const deleteUserMany = await prisma.users.deleteMany({
+        where: {
+            nameUser: 'Juana'
+        }
+    })
+    console.log(deleteUserMany) */
 }
 
 
